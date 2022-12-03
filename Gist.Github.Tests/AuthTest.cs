fbc6328ac7e95494204711c48490a7d7a8033ffc
Lipatov Alexander <lipatov.work@bk.ru>
2022-12-03 18:01:28 +0300

    Refactor

--- Gist.Github.Tests/AuthTest.cs
+++ Gist.Github.Tests/AuthTest.cs
@@ -1,36 +1,53 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace Gist.Github.Tests;
 
 [TestFixture]
 public class AuthTest {
-    private IWebDriver driver;
-    public IDictionary<string, object> vars {get; private set;}
-    private IJavaScriptExecutor js;
+    private IWebDriver _driver = null!;
+    private WebDriverWait _wait = null!;
     
     [SetUp]
     public void SetUp() {
-        driver = new ChromeDriver();
-        js = (IJavaScriptExecutor)driver;
-        vars = new Dictionary<string, object>();
+        _driver = new ChromeDriver();
+        _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
     }
     
     [TearDown]
     protected void TearDown() {
-        driver.Quit();
+        _driver.Close();
+        _driver.Quit();
     }
     
     [Test]
     public void Auth() {
-        driver.Navigate().GoToUrl("https://gist.github.com/starred");
-        driver.Manage().Window.Size = new System.Drawing.Size(947, 816);
-        driver.FindElement(By.LinkText("Sign in")).Click();
-        Thread.Sleep(1000);
-        driver.FindElement(By.Id("login_field")).SendKeys("githubtests-itis");
-        driver.FindElement(By.Id("password")).SendKeys("g1thubTests");
-        driver.FindElement(By.Name("commit")).Click();
-        Thread.Sleep(1000);
-        driver.Close();
+        OpenHomePage();
+        Login();
+        Thread.Sleep(3000);
+    }
+
+    private void Login()
+    {
+        _driver
+            .FindElement(By.LinkText("Sign in"))
+            .Click();
+        _wait
+            .Until(d => d.FindElement(By.Id("login_field")))
+            .SendKeys("githubtests-itis");
+        _driver
+            .FindElement(By.Id("password"))
+            .SendKeys("g1thubTests");
+        _driver
+            .FindElement(By.Name("commit"))
+            .Click();
+    }
+
+    private void OpenHomePage()
+    {
+        _driver
+            .Navigate()
+            .GoToUrl("https://gist.github.com/starred");
     }
 }
