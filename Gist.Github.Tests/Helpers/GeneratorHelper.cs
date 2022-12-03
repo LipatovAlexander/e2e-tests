abee44dfc8c071fef93239f1c34538e5887caf08
Lipatov Alexander <lipatov.work@bk.ru>
2022-12-03 22:39:00 +0300

    Test with generated data

---
+++ Gist.Github.Tests/Helpers/GeneratorHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using Gist.Github.Model;
+
+namespace Gist.Github.Helpers;
+
+public sealed class GeneratorHelper
+{
+    private const string FilePath =
+        "C:\\Users\\pc\\source\\repos\\Tests\\GistGenerator\\bin\\Debug\\net7.0\\gists.json";
+
+    public static IReadOnlyCollection<GistData> ReadGeneratedGists()
+    {
+        var json = File.ReadAllText(FilePath);
+
+        return JsonSerializer.Deserialize<GistData[]>(json)!;
+    }
+}
--- Gist.Github.Tests/Tests/CrudTests.cs
+++ Gist.Github.Tests/Tests/CrudTests.cs
@@ -1,53 +1,46 @@
-﻿using Gist.Github.Model;
+﻿using Gist.Github.Helpers;
+using Gist.Github.Model;
 
 namespace Gist.Github.Tests;
 
 public class CrudTests : TestBase
 {
     private string _username = null!;
-    private GistData _gist = null!; 
+    public static readonly IReadOnlyCollection<GistData> Gists = GeneratorHelper.ReadGeneratedGists();
 
     [OneTimeSetUp]
     public void SetUp()
     {
         _username = Application.Auth.GetUsername()
             ?? throw new InvalidOperationException();
-            
-        var guid = Guid.NewGuid().ToString();
-        _gist = new GistData
-        {
-            FileName = $"file_{guid}",
-            Description = $"description ${guid}",
-            Content = $"content {guid}"
-        };
     }
 
-    [Test]
-    public void Create()
+    [Test, TestCaseSource(nameof(Gists))]
+    public void Create(GistData gist)
     {
         // Arrange
 
         // Act
-        Application.Gist.CreateGist(_gist);
-        var actualGist = Application.Gist.GetGist(_username, _gist.Id)!;
+        Application.Gist.CreateGist(gist);
+        var actualGist = Application.Gist.GetGist(_username, gist.Id)!;
 
         // Assert
         Assert.NotNull(actualGist);
-        Assert.AreEqual(_gist.Description, actualGist.Description);
-        Assert.AreEqual(_gist.FileName, actualGist.FileName);
-        Assert.AreEqual(_gist.Content, actualGist.Content);
+        Assert.AreEqual(gist.Description, actualGist.Description);
+        Assert.AreEqual(gist.FileName, actualGist.FileName);
+        Assert.AreEqual(gist.Content, actualGist.Content);
     }
 
-    [Test]
-    public void Delete()
+    [Test, TestCaseSource(nameof(Gists))]
+    public void Delete(GistData gist)
     {
         // Arrange
 
         // Act
-        Application.Gist.DeleteGist(_username, _gist.Id);
+        Application.Gist.DeleteGist(_username, gist.Id);
 
         // Assert
-        var deletedGist = Application.Gist.GetGist(_username, _gist.Id);
+        var deletedGist = Application.Gist.GetGist(_username, gist.Id);
         Assert.Null(deletedGist);
     }
 }
