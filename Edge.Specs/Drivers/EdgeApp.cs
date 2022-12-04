cdf794471cc984381201b7a9507694aeb10865d0
Lipatov Alexander <lipatov.work@bk.ru>
2022-12-04 05:36:56 +0300

    Add wait

--- Edge.Specs/Drivers/EdgeApp.cs
+++ Edge.Specs/Drivers/EdgeApp.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Threading;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using SpecFlow.Actions.WindowsAppDriver;
 
 namespace Edge.Specs.Drivers;
@@ -7,27 +9,37 @@ namespace Edge.Specs.Drivers;
 public sealed class EdgeApp : IDisposable
 {
     private readonly AppDriver _appDriver;
+    private readonly DefaultWait<AppDriver> _wait;
 
     public EdgeApp(AppDriver appDriver)
     {
         _appDriver = appDriver;
+        _wait = new DefaultWait<AppDriver>(_appDriver);
+        _wait.IgnoreExceptionTypes(typeof(WebDriverException));
     }
 
     public void OpenNewTab()
     {
-        _appDriver.Current.FindElementByName("Новая вкладка").Click();
+        _wait
+            .Until(driver => driver.Current.FindElementByName("Новая вкладка"))
+            .Click();
     }
 
     public string GetCurrentTabName()
     {
-        return _appDriver.Current.FindElementByTagName("Document").GetAttribute("Name");
+        return _wait
+            .Until(driver => driver.Current.FindElementByTagName("Document"))
+            .GetAttribute("Name");
     }
 
     public void GoTo(string address)
     {
-        var searchPanel = _appDriver.Current.FindElementByName("Адресная строка и панель поиска");
+        var searchPanel = _wait.Until(driver => driver.Current.FindElementByName("Адресная строка и панель поиска"));
+        
+        searchPanel.Clear();
         searchPanel.SendKeys(address);
         searchPanel.SendKeys(Keys.Enter);
+        Thread.Sleep(1000);
     }
 
     public void Dispose()
