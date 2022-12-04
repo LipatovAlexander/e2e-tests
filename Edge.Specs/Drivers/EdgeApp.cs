7bf7ffdf9a7991dad4a917d3f503b3bdf31891f4
Lipatov Alexander <lipatov.work@bk.ru>
2022-12-04 04:25:41 +0300

    Add desktop tests

---
+++ Edge.Specs/Drivers/EdgeApp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using OpenQA.Selenium;
+using SpecFlow.Actions.WindowsAppDriver;
+
+namespace Edge.Specs.Drivers;
+
+public sealed class EdgeApp : IDisposable
+{
+    private readonly AppDriver _appDriver;
+
+    public EdgeApp(AppDriver appDriver)
+    {
+        _appDriver = appDriver;
+    }
+
+    public void OpenNewTab()
+    {
+        _appDriver.Current.FindElementByName("Новая вкладка").Click();
+    }
+
+    public string GetCurrentTabName()
+    {
+        return _appDriver.Current.FindElementByTagName("Document").GetAttribute("Name");
+    }
+
+    public void GoTo(string address)
+    {
+        var searchPanel = _appDriver.Current.FindElementByName("Адресная строка и панель поиска");
+        searchPanel.SendKeys(address);
+        searchPanel.SendKeys(Keys.Enter);
+    }
+
+    public void Dispose()
+    {
+        _appDriver?.Dispose();
+    }
+}
---
+++ Edge.Specs/Hooks/EdgeHooks.cs
@@ -0,0 +1,23 @@
+﻿using Edge.Specs.Drivers;
+using SpecFlow.Actions.WindowsAppDriver;
+using TechTalk.SpecFlow;
+
+namespace Edge.Specs.Hooks;
+
+[Binding]
+public sealed class EdgeHooks
+{
+    public static EdgeApp Edge; 
+
+    [BeforeTestRun]
+    public static void BeforeTestRun(AppDriver driver)
+    {
+        Edge = new EdgeApp(driver);
+    }
+
+    [AfterTestRun]
+    public static void AfterTestRun()
+    {
+        Edge.Dispose();
+    }
+}
